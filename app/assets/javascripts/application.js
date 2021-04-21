ab0a71b2f673ed524495fb726ba18cde1acb1d2d
Stephen <stephen.sjbrown@gmail.com>
2021-04-21 14:01:11 +0100

    usability update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,11 @@ const caseData = {
     changes: {
       chng0: ["Change phone number", "blue", "received"],
       chng1: ["Change address", "blue", "received"],
-      chng2: ["Change to Direct Pay or Collect and Pay", "yellow", "in progress"],
+      chng2: [
+        "Change to Direct Pay or Collect and Pay",
+        "yellow",
+        "in progress",
+      ],
     },
   },
   janet: {
@@ -44,7 +48,11 @@ const caseData = {
       msg2: ["We have been updated of special expenses", "10/11/2020"],
     },
     changes: {
-      chng0: ["Change to Direct Pay or Collect and Pay", "yellow", "in progress"],
+      chng0: [
+        "Change to Direct Pay or Collect and Pay",
+        "yellow",
+        "in progress",
+      ],
       chng1: ["Update shared care", "green", "Accepted"],
       chng2: ["Update shared care", "green", "Accepted"],
     },
@@ -59,80 +67,79 @@ const caseData = {
     changes: {
       chng0: ["Update income", "yellow", "in progress"],
       chng1: ["Child still in education", "green", "Accepted"],
-      chng2: ["Update shared care", "green","Accepted"],
+      chng2: ["Update shared care", "green", "Accepted"],
     },
   },
 };
 
-let message0 = $("#message0")[0]
-let message1 = $("#message1")[0]
-let message2 = $("#message2")[0]
-let date0 = $("#date0")[0]
-let date1 = $("#date1")[0]
-let date2 = $("#date2")[0]
-let change0 = $("#change0")[0]
-let change1 = $("#change1")[0]
-let change2 = $("#change2")[0]
-let tag0 = $("#tag0")[0]
-let tag1 = $("#tag1")[0]
-let tag2 = $("#tag2")[0]
+let message0 = $("#message0")[0];
+let message1 = $("#message1")[0];
+let message2 = $("#message2")[0];
+let date0 = $("#date0")[0];
+let date1 = $("#date1")[0];
+let date2 = $("#date2")[0];
+let change0 = $("#change0")[0];
+let change1 = $("#change1")[0];
+let change2 = $("#change2")[0];
+let tag0 = $("#tag0")[0];
+let tag1 = $("#tag1")[0];
+let tag2 = $("#tag2")[0];
 
-
-const changeData = caseName => {
-  let newData = caseData[caseName]
+const changeData = (caseName) => {
+  let newData = caseData[caseName];
   // console.log(newData)
-  message0.innerText = newData.messages.msg0[0]
-  message1.innerText = newData.messages.msg1[0]
-  message2.innerText = newData.messages.msg2[0]
-  date0.innerText = newData.messages.msg0[1]
-  date1.innerText = newData.messages.msg1[1]
-  date2.innerText = newData.messages.msg2[1]
-  change0.innerText = newData.changes.chng0[0]
-  change1.innerText = newData.changes.chng1[0]
-  change2.innerText = newData.changes.chng2[0]
-  tag0.classList.remove(...tag0.classList)
-  tag1.classList.remove(...tag1.classList)
-  tag2.classList.remove(...tag2.classList)
-  tag0.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng0[1]}`)
-  tag1.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng1[1]}`)
-  tag2.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng2[1]}`)
-  tag0.innerText = newData.changes.chng0[2]
-  tag1.innerText = newData.changes.chng1[2]
-  tag2.innerText = newData.changes.chng2[2]
-}
+  message0.innerText = newData.messages.msg0[0];
+  message1.innerText = newData.messages.msg1[0];
+  message2.innerText = newData.messages.msg2[0];
+  date0.innerText = newData.messages.msg0[1];
+  date1.innerText = newData.messages.msg1[1];
+  date2.innerText = newData.messages.msg2[1];
+  change0.innerText = newData.changes.chng0[0];
+  change1.innerText = newData.changes.chng1[0];
+  change2.innerText = newData.changes.chng2[0];
+  tag0.classList.remove(...tag0.classList);
+  tag1.classList.remove(...tag1.classList);
+  tag2.classList.remove(...tag2.classList);
+  tag0.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng0[1]}`);
+  tag1.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng1[1]}`);
+  tag2.classList.add("govuk-tag", `govuk-tag--${newData.changes.chng2[1]}`);
+  tag0.innerText = newData.changes.chng0[2];
+  tag1.innerText = newData.changes.chng1[2];
+  tag2.innerText = newData.changes.chng2[2];
+};
 
 const showCase = (e) => {
-  console.log(e.target.value)
+  console.log(e.target.value);
   switch (e.target.value) {
     case "janet":
       $("#janet").show();
       $("#clive").hide();
       $("#frankie").hide();
-      changeData(e.target.value)
+      changeData(e.target.value);
       break;
     case "clive":
       $("#clive").show();
       $("#janet").hide();
       $("#frankie").hide();
-      changeData(e.target.value)
+      changeData(e.target.value);
       break;
     case "frankie":
       $("#frankie").show();
       $("#clive").hide();
       $("#janet").hide();
-      changeData(e.target.value)
+      changeData(e.target.value);
       break;
     default:
       $("#clive").show();
       $("#janet").show();
       $("#frankie").show();
-      changeData("all")
+      changeData("all");
       break;
   }
 };
 
 const showCalcs = (e) => {
-  console.log(e.target.value)
+  console.log(e.target.value);
   switch (e.target.value) {
     case "janet":
       $("#janet").show();
@@ -149,11 +156,15 @@ const showCalcs = (e) => {
       $("#frankie").hide();
       $("#janet").hide();
       break;
-  }
+    }
 };
 
+// $("#janet").show();
+// $("#all-cases").hide();
+// $("#frankie").hide();
+
 const showPayments = (e) => {
-  console.log(e.target.value)
+  console.log(e.target.value);
   switch (e.target.value) {
     case "case1":
       $("#case1").show();
@@ -174,24 +185,17 @@ const showPayments = (e) => {
 };
 
 const sorter = $("#sort")[0];
-const calcSorter = $("#calcs-sort")[0]
-const paySorter = $("#payment-sort")[0]
+const calcSorter = $("#calcs-sort")[0];
+const paySorter = $("#payment-sort")[0];
 
 try {
   sorter.addEventListener("change", showCase);
-}
-catch(err) {
-}
+} catch (err) {}
 
 try {
   calcSorter.addEventListener("change", showCalcs);
-}
-catch(err) {
-}
+} catch (err) {}
 
 try {
   paySorter.addEventListener("change", showPayments);
-}
-catch(err) {
-}
-
+} catch (err) {}
